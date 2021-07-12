590997aae56ecd12bef75ba06f3e3076ffb6b9fe
Vytautas <vytautaskaralius@gmail.com>
2021-07-12 10:02:00 +0300

    Added prediction alghorithm check functionality

--- Predictor.py
+++ Predictor.py
@@ -50,29 +50,44 @@ def game_core_v3(number_to_predict, range_start=1, range_end=100,
         prev_range_start = found_sub_range[0][0]
         prev_range_end = found_sub_range[0][1]
 
+    found_number = -1
+
     # ищем в последнем найденом субинтервале угадываемое число
     for number in range(found_sub_range[0][0], found_sub_range[0][1] + 1):
         tries_count += 1
         if number == number_to_predict: # число найдено - прерываем цикл
+            found_number = number
             break
 
-    # возвращаем количество попыток
-    return tries_count
+    # возвращаем найденное число и количество попыток
+    return found_number, tries_count
 
 
 def score_game(game_core, range_start=1, range_end=100, ranges_count=2):
     """Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число"""
     count_ls = []
+    tried_numbers = []
 
     # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
     np.random.seed(1)
     random_array = np.random.randint(range_start, range_end + 1, size=1000)
+
     for number in random_array:
-        count_ls.append(game_core(number, range_start, range_end,
-                                  ranges_count))
+        # ищем число
+        prediction_result = game_core(number, range_start, range_end, ranges_count)
+        # запоминаем сгенерированное и угаданное числа
+        predicted_numbers = (number, prediction_result[0])
+        tried_numbers.append(predicted_numbers)
+
+        # запоминаем количество попыток
+        count_ls.append(prediction_result[1])
+
     # вычисляем среднее значение попыток
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток. Количество подинтервалов: {ranges_count}")
+    print("Найдено элементов, где угаданное число не совпадает со случайно сгенерированным (должно быть ноль "
+          "элементов): " +
+          str(list(filter(lambda x: x[0] != x[1],tried_numbers))))
     return score
 
 
@@ -82,8 +97,8 @@ ranges_and_tries = []
 # перебираем количество интервалов от одного до ста
 # и вычисляем количество попыток
 for ranges_count in range(1, 101):
-    ranges_and_tries.append((ranges_count,
-        score_game(game_core_v3, range_end=100, ranges_count=ranges_count)))
+    ranges_and_tries.append((ranges_count, score_game(game_core_v3, range_end=100, ranges_count=ranges_count)))
+
 
 # ищем запись с минимальным значением попыток
 min_tries = list(sorted(ranges_and_tries,key= lambda x: x[1]))[0]
